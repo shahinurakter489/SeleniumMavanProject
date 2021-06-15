e1b96a7d85bd9cf726ba7e270accbcac8e1a7626
shahin <shahinurakter085bd@gmail.com>
2021-06-15 18:27:36 -0400

    six commit

---
+++ src/test/java/mavanPackage/Flower.java
@@ -0,0 +1,10 @@
+package mavanPackage;
+
+public class Flower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/mavanPackage/MyMavanClass.java
@@ -0,0 +1,10 @@
+package mavanPackage;
+
+public class MyMavanClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
